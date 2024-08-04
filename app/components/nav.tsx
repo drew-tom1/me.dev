8b3911c8863770448d1a0c78921940d98177c9d6
drew-tom1 <tom.drew261@gmail.com>
2024-08-03 18:55:22 -0700

    text change to GeistMono + misc ui

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -29,7 +29,7 @@ export function Navbar() {
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-cyan-200
+                  className="transition-all hover:text-neutral-800 dark:hover:text-cyan-200 hover:scale-105
                    flex align-middle relative py-1 px-2 m-1"
                 >
                   {name}
--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,17 @@ export default function Page() {
           words={["hi, i'm drew!"]}
           cursor
           cursorBlinking
-          typeSpeed={150}
-          deleteSpeed={125}
+          typeSpeed={80}
+          deleteSpeed={50}
           delaySpeed={1000}
-          loop={0}
         />
       </h1>
       <hr />
       <p className="my-4">
         {`I'm a second year at San Jose State University studying cybersecurity and software engineering.
-        I'm apart of some clubs on campus including SJSU's Software and Computer Engineering Society. I love learning about new things.
-        In my freetime, I love playing with my cats, learning about investments, or nerding out over Warhammer 40,000!`}
+        I'm a part of some clubs on campus including SJSU's `}
+        <a href="https://www.sce.sjsu.edu/" target="_blank" rel="noopener noreferrer" className="hover:scale-105 dark:text-cyan-200 hover:underline">Software and Computer Engineering Society</a>
+        {`. I love learning about new things. In my free time, I love playing with my cats, learning about investments, or nerding out over Warhammer 40,000!`}
       </p>
       <div className="my-4">
         <BlogPosts />
