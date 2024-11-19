73b98a13d59df427e57a64d81ab34eacdf21fff3
drew-tom1 <tom.drew261@gmail.com>
2024-11-18 19:12:00 -0800

    added investment 3, reformatting with blog display.

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -22,10 +22,10 @@ export function BlogPosts() {
             href={`/blog/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
+              <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
+                {formatDate(post.metadata.publishedAt, true)}
               </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight hover:scale-105 hover:text-red-400 duration-300 dark:hover:text-cyan-300">
                 {post.metadata.title}
               </p>
             </div>
