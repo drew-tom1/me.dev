bc206f7157bdba890d158cea0fd04523a15b66e0
drew-tom1 <tom.drew261@gmail.com>
2025-04-26 22:26:47 -0700

    timeline component

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -31,7 +31,7 @@ export function Navbar() {
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
+          <div className="flex flex-row space-x-0 pr-10 border rounded-lg border-gray-700">
             {Object.entries(navItems).map(([path, { name }]) => {
               const isActive = pathname === path;
               const linkClass = `transition-all hover:text-red-400 dark:hover:text-cyan-300 hover:scale-105 flex align-middle relative py-1 px-2 m-1 ${isActive ? 'scale-110 text-red-400 dark:text-cyan-300' : ''}`;
--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -8,28 +8,41 @@ import 'react-vertical-timeline-component/style.min.css';
 
 export default function Page() {
   return (
-    <VerticalTimeline>
-      <VerticalTimelineElement visible={true}>
-        <h3>Creative Director</h3>
-        <h4>Miami, FL</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-        </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement visible={true}>
-        <h3>Creative Director</h3>
-        <h4>Miami, FL</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-        </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement visible={true}>
-        <h3>Creative Director</h3>
-        <h4>Miami, FL</h4>
-        <p>
-          Creative Direction, User Experience, Visual Design, Project Management, Team Leading
-        </p>
-      </VerticalTimelineElement>
-    </VerticalTimeline>
+    <section>
+      <h1 className="font-semibold text-4xl mb-6 tracking-tighter">
+        work experiences!
+      </h1>
+      <hr className="border-gray-700 mb-6" />
+      <VerticalTimeline layout="1-column">
+        <VerticalTimelineElement visible={true} contentStyle={{ background: 'var(--color-zinc-900)', color: '#fff', border: 'solid', borderWidth: '1px'}} date="September 2024 - January 2025" dateClassName="text-center">
+          <h3>Software Engineering Intern</h3>
+          <h4>Arctex Inc</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement visible={true} contentStyle={{ background: 'var(--color-zinc-900)', color: '#fff', border: 'solid', borderWidth: '1px'}} date="September 2024 - January 2025" dateClassName="text-center">
+          <h3>Creative Director</h3>
+          <h4>Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement visible={true} contentStyle={{ background: 'var(--color-zinc-900)', color: '#fff', border: 'solid', borderWidth: '1px'}} date="September 2024 - January 2025" dateClassName="text-center">
+          <h3>Creative Director</h3>
+          <h4>Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement visible={true} contentStyle={{ background: 'var(--color-zinc-900)', color: '#fff', border: 'solid', borderWidth: '1px'}} date="September 2024 - January 2025" dateClassName="text-center">
+          <h3>Creative Director</h3>
+          <h4>Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </section>
   )
 };
