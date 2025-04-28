6a40e791535e921bfc7c53dc3a96d98bdd2f1efe
drew-tom1 <tom.drew261@gmail.com>
2025-04-28 14:18:41 -0700

    working prototype

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -5,6 +5,10 @@ import "../global.css";
 import { hobbyItems, projectItems, resumeItems } from "./items";
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
+import usaa from "./photos/usaa.jpg";
+import arctex from './photos/arctex.png';
+import sjsu from './photos/sjsu.png';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -14,7 +18,7 @@ export default function Page() {
       </h1>
       <hr className="border-gray-700 mb-6" />
       <VerticalTimeline layout="1-column">
-        <VerticalTimelineElement visible={true} contentStyle={{ background: 'var(--color-zinc-900)', color: '#fff', border: 'solid', borderWidth: '1px'}} date="May 2025 - August 2025" dateClassName="text-center">
+        <VerticalTimelineElement icon={<Image src={usaa} alt="USAA Logo" width={40} height={40} className="rounded-2xl"/>} visible={true} contentStyle={{ background: 'var(--color-zinc-900)', color: '#fff', border: 'solid', borderWidth: '1px'}} date="May 2025 - August 2025" dateClassName="text-center">
           <h3>Incoming Software Engineering Intern</h3>
           <h4>USAA</h4>
           <p>
@@ -24,7 +28,7 @@ export default function Page() {
           <li>JavaScript w/ React.js</li>
           <li>Agile/Scrum methodologies and SDLC</li>
         </VerticalTimelineElement>
-        <VerticalTimelineElement visible={true} contentStyle={{ background: 'var(--color-zinc-900)', color: '#fff', border: 'solid', borderWidth: '1px'}} date="September 2024 - January 2025" dateClassName="text-center">
+        <VerticalTimelineElement icon={<Image src={arctex} alt="USAA Logo" width={40} height={40} className="rounded-2xl"/>} visible={true} contentStyle={{ background: 'var(--color-zinc-900)', color: '#fff', border: 'solid', borderWidth: '1px'}} date="September 2024 - January 2025" dateClassName="text-center">
           <h3>Software Engineering Intern</h3>
           <h4>Arctex Inc</h4>
           <p>
@@ -34,14 +38,14 @@ export default function Page() {
           <li>RESTful APIs w/ Express.js and MongoDB</li>
           <li>API Testing w/ Postman</li>
         </VerticalTimelineElement>
-        <VerticalTimelineElement visible={true} contentStyle={{ background: 'var(--color-zinc-900)', color: '#fff', border: 'solid', borderWidth: '1px'}} date="September 2024 - January 2025" dateClassName="text-center">
+        <VerticalTimelineElement icon={<Image src={sjsu} alt="USAA Logo" width={40} height={40} className="rounded-2xl"/>} visible={true} contentStyle={{ background: 'var(--color-zinc-900)', color: '#fff', border: 'solid', borderWidth: '1px'}} date="September 2024 - January 2025" dateClassName="text-center">
           <h3>Software Engineering Intern</h3>
           <h4>San Jose State University</h4>
           <p>
             Development Team
           </p>
-          <li></li>
-          <li></li>
+          <li>Python w/ FastAPI</li>
+          <li>JavaScript w/ React.js</li>
           <li></li>
         </VerticalTimelineElement>
       </VerticalTimeline>
---
+++ app/types/images.d.ts
@@ -0,0 +1,26 @@
+declare module '*.jpg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.jpeg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.png' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.gif' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.svg' {
+    import React = require('react');
+    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
+  }
